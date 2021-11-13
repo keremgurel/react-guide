1c9f19f314e4e8e7b3edcecb8b8122125ca536e5
Kerem Gurel <keremgurell99@gmail.com>
2021-11-12 22:31:21 -0800

    Conditional AddExpense Form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,83 +2,105 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
-  const [title, setTitle] = useState('');
-  const [amount, setAmount] = useState('');
-  const [date, setDate] = useState('');
+	const [title, setTitle] = useState('');
+	const [amount, setAmount] = useState('');
+	const [date, setDate] = useState('');
+	const [show, setShow] = useState(false);
 
-  // const [userInput, setUserInput] = useState({
-  //   title: '',
-  //   amount: '',
-  //   date: '',
-  // });
+	// const [userInput, setUserInput] = useState({
+	//   title: '',
+	//   amount: '',
+	//   date: '',
+	// });
 
-  // const handleTitleChange = (event) => {
-  //   const setUserInput((prevState) => {
-  //     return { ...prevState, title: event.target.value }
-  //   })
-  // }
+	// const handleTitleChange = (event) => {
+	//   const setUserInput((prevState) => {
+	//     return { ...prevState, title: event.target.value }
+	//   })
+	// }
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
+	const handleTitleChange = (event) => {
+		setTitle(event.target.value);
+	};
 
-  const handleAmountChange = (event) => {
-    setAmount(event.target.value);
-  };
+	const handleAmountChange = (event) => {
+		setAmount(event.target.value);
+	};
 
-  const handleDateChange = (event) => {
-    setDate(event.target.value);
-  };
+	const handleDateChange = (event) => {
+		setDate(event.target.value);
+	};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+	const handleCancelClick = (e) => {
+		e.preventDefault();
+		setShow(false);
+	};
 
-    const expenseData = {
-      title: title,
-      amount: amount,
-      date: new Date(date),
-    };
+	const handleSubmit = (event) => {
+		event.preventDefault();
 
-    props.onSaveExpenseData(expenseData);
+		const expenseData = {
+			title: title,
+			amount: amount,
+			date: new Date(date),
+		};
 
-    setTitle('');
-    setAmount('');
-    setDate('');
-  };
+		props.onSaveExpenseData(expenseData);
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
-          <label>Title</label>
-          <input type='text' value={title} onChange={handleTitleChange} />
-        </div>
-        <div className='new-expense__control'>
-          <label>Amount</label>
-          <input
-            type='number'
-            min='0.01'
-            step='0.01'
-            onChange={handleAmountChange}
-            value={amount}
-          />
-        </div>
-        <div className='new-expense__control'>
-          <label>Date</label>
-          <input
-            type='date'
-            min='2019-02-02'
-            max='2022-12-31'
-            onChange={handleDateChange}
-            value={date}
-          />
-        </div>
-      </div>
-      <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
-      </div>
-    </form>
-  );
+		setTitle('');
+		setAmount('');
+		setDate('');
+		setShow(false);
+	};
+
+	const handleAddExpenseClick = () => {
+		setShow(true);
+	};
+
+	if (show === false) {
+		return (
+			<button type='submit' onClick={handleAddExpenseClick}>
+				Add New Expense
+			</button>
+		);
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<div className='new-expense__controls'>
+				<div className='new-expense__control'>
+					<label>Title</label>
+					<input type='text' value={title} onChange={handleTitleChange} />
+				</div>
+				<div className='new-expense__control'>
+					<label>Amount</label>
+					<input
+						type='number'
+						min='0.01'
+						step='0.01'
+						onChange={handleAmountChange}
+						value={amount}
+					/>
+				</div>
+				<div className='new-expense__control'>
+					<label>Date</label>
+					<input
+						type='date'
+						min='2019-02-02'
+						max='2022-12-31'
+						onChange={handleDateChange}
+						value={date}
+					/>
+				</div>
+			</div>
+			<div className='new-expense__actions'>
+				<button type='cancel' onClick={handleCancelClick}>
+					Cancel
+				</button>
+				<button type='submit'>Add Expense</button>
+			</div>
+		</form>
+	);
 };
 
 export default ExpenseForm;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,21 +3,21 @@ import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
-  const handleSaveExpenseData = (enteredExpenseData) => {
-    const expenseData = {
-      ...enteredExpenseData,
-      id: Math.random().toString(),
-    };
-    console.log('Logging ExpenseForm data in NewExpense', expenseData);
+	const handleSaveExpenseData = (enteredExpenseData) => {
+		const expenseData = {
+			...enteredExpenseData,
+			id: Math.random().toString(),
+		};
+		console.log('Logging ExpenseForm data in NewExpense', expenseData);
 
-    props.onAddExpense(expenseData);
-  };
+		props.onAddExpense(expenseData);
+	};
 
-  return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={handleSaveExpenseData} />
-    </div>
-  );
+	return (
+		<div className='new-expense'>
+			<ExpenseForm onSaveExpenseData={handleSaveExpenseData} />
+		</div>
+	);
 };
 
 export default NewExpense;
